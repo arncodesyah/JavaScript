89d8624b21ca8a6ec6e897240af1bebcbe2e1b99
arncodesyah <149872829+arncodesyah@users.noreply.github.com>
2026-04-23 17:23:21 +0200

    Add files via upload

---
+++ CallStack.js
@@ -0,0 +1,12 @@
+function one(){
+    console.log("1")
+}
+function two(){
+    console.log("2")
+}
+function three(){
+    console.log("3")
+}
+one()
+two()
+three()
---
+++ method.js
@@ -0,0 +1,4 @@
+let planes=["B737","A380","F15","A321neo"]
+//planes.push("Lockheed Martin")
+//planes.pop()
+console.log(planes)
