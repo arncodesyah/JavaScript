34eac1b089c7b1e77b00db3e568f5cddea14c09c
arncodesyah <149872829+arncodesyah@users.noreply.github.com>
2026-05-16 21:29:24 +0200

    Add files via upload

---
+++ err.js
@@ -0,0 +1,13 @@
+let num = 3464592754754
+let num2 = 0
+
+try {
+    if (num2 == 0) {
+        throw "Did you pass kindergarten!!! You know that you cant divide by zero🧠!! Go back to school"
+    }
+    console.log(num/num2)
+
+}
+catch(error){
+    console.log(error)
+}
---
+++ type.js
@@ -0,0 +1,10 @@
+//const num1=2344
+//let x=String(num1)
+//console.log(x)
+//console.log(typeof x)
+
+const marks="92100"
+let x2=Number(marks)
+console.log(x2)
+console.log(typeof x2)
+
